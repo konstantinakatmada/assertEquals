64763ed1c1418c99f6999edfe09eb294927ff264
roland.ormrod <roland.ormrod@fluidly.com>
2020-09-17 17:31:03 +0100

    Adds requirements to readme

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 
 # Fluidly home and pairing test
 
-## Scope
-⚠️ Please implement the first 5 tests in the provided spec file only.
 
-️⚠️ There is a large divider which will clearly tell you when to stop implementing at home. We will get the final tests passing together at a later time.
 
 ## Terms of the exercise
 
 * Please do not use external libraries.
-* We expect you should be able to get the first 5 tests passing at home within an hour
+* We expect you should be able to get the requirements below implemented within an hour. 
+* We will implement assertEquals for Object comparison together in the remote pairing session, so if you want to plan for this in your approach you are welcome to do so
 * You may use online resources to assist you with specific techniques, syntax etc. but please do not just copy code.
 * Please don't share this exercise with any third party
 
@@ -22,3 +20,14 @@ The aim of the exercise is to demonstrate your problem solving and understanding
 * You may add more functions.
 * Credit will be given for approach, correctly identifying "failed" assertEquals, clean, testable code and coding style.
 * The set of tests provided isn't exhaustive - there are cases that they don't handle.
+
+
+## Assert Equal Requirements list
+
+| Expected   |      Actual      |  Result |
+|----------|:-------------:|------:|
+| "abc" |  "abc" | Function does not throw error |
+| "abcef" |  "abc" | Throws error with message 'Expected "abcef" but found "abc"' |
+| ['a'] |  {0: 'a'} | Throws error with message 'Expected type Array but found type Object'' |
+| ['a', 'b', 'c'] |  ['a', 'b', 'c'] | Function does not throw error |
+| ['a', 'b'] |  ['a', 'b', 'c'] | Throws error with message 'Expected array length 2 but found 3' |
--- src/index.spec.js
+++ src/index.spec.js
@@ -24,6 +24,7 @@ var complexObject2 = {
     propC: 2
   }
 };
+
 var complexObject3 = {
   propA: 1,
   propB: {
@@ -35,52 +36,5 @@ var complexObject3 = {
 describe('assertEquals', () => {
   it('doesnt throw when 2 strings that are equal are compared', () => {
     expect(() => assertEquals('abc', 'abc')).not.toThrow()
-  })
-
-  it('throws when two different strings are compared', () => {
-    expect(() => assertEquals('abcef', 'abc')).toThrow('Expected "abcef" found "abc"')
-  })
-
-  it('throws when presented with an array compared against an object', () => {
-    expect(() => assertEquals(['a'], {0: 'a'})).toThrow('Expected type Array but found type Object')
-  })
-
-  it('doesnt throw when two equal arrays are compared', () => {
-    expect(() => assertEquals(['a', 'b', 'c'], ['a', 'b', 'c'])).not.toThrow()
-  })
-
-  it('throws when two arrays of different length are compared', () => {
-    expect(() => assertEquals(['a', 'b'], ['a', 'b', 'c'])).toThrow('Expected array length 2 but found 3')
-  })
-
-  // ########################################################################################
-  // ########################################################################################
-  // ########################################################################################
-  // ########################################################################################
-  // ###########################                                  ###########################
-  // ###########################           STOP HERE!!!!!         ###########################
-  // ###########################   Please leave the below tests   ###########################
-  // ###########################      for the remote pairing      ###########################
-  // ###########################              session             ###########################
-  // ###########################                                  ###########################
-  // ########################################################################################
-  // ########################################################################################
-  // ########################################################################################
-  // ########################################################################################
-
-  it('doesnt throw when two objects that are the same structure are compared', () => {
-    expect(() => assertEquals(complexObject1, complexObject1Copy)).not.toThrow()
-  })
-
-  it('throws when 2 different objects are compared with the same structure but a nested key is different', () => {
-    expect(() => assertEquals(complexObject1, complexObject2)).toThrow('Expected propB.propA[1].propB "b" but found "c"')
-  })
-
-  it('throws when 2 different objects are compared where the actual is missing a key', () => {
-    expect(() => assertEquals(complexObject1, complexObject3)).toThrow('Expected propB.propC but was not found')
-  })
-
-  it('throws when null is compared with an empty object', () => {
-    expect(() => assertEquals(null, {})).toThrow('Expected type Null but found type Object')
   })
 })
